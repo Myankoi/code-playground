aa1dba6d033fe27936988e8f08852d31b7324af2
Myan <146714203+Myankoi@users.noreply.github.com>
2024-08-10 16:10:06 +0700

    testing

---
+++ #2/#2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    internal class Program
+    {
+        // Tugas Ramadian ❤
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nama kamu ciapah?");
+            string username = Console.ReadLine();
+            Console.WriteLine($" jelek {username} Hai ");
+
+            Console.WriteLine("Umur kamu belapah");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("tuwa lu");
+        }
+    }
+}
