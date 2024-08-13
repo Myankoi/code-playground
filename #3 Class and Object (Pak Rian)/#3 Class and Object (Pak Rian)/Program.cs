d0a0dd68294ba4cd2bff3e905656d5515e594a5f
Myan <146714203+Myankoi@users.noreply.github.com>
2024-08-13 17:44:23 +0700

    init

---
+++ #3 Class and Object (Pak Rian)/#3 Class and Object (Pak Rian)/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_Class_and_Object__Pak_Rian_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
