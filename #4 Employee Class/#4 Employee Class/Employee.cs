4cf5018cd06218fb5d39aa0347c054c8ed22ba56
Myan <146714203+Myankoi@users.noreply.github.com>
2024-08-19 22:23:50 +0700

    ✨done✨

---
+++ #4 Employee Class/#4 Employee Class/Employee.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_Employee_Class
+{
+    public class Employee
+    {
+        private int id, salary;
+        private string firstName, lastName;
+        public Employee(int id, string firstName, string lastName, int salary)
+        {
+            this.id = id;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.salary = salary;
+        }
+
+        public int getID()
+        {
+            return id;
+        }
+
+        public string getFirstName()
+        {
+            return firstName;
+        }
+
+        public string getLastName()
+        {
+            return lastName;
+        }
+
+        public string getName()
+        {
+            return firstName + " " + lastName;
+        }
+
+        public int getSalary() 
+        {
+            return salary;
+        }
+
+        public void setSalary(int salary) 
+        {
+            this.salary = salary;
+        }
+        public int getAnnualSalary()
+        {
+            return (salary * 12);
+        }
+        public int raiseSalary(int percent) 
+        {
+            return (salary + (salary * percent/100));
+        }
+        public string toString()
+        {
+            return $"Employee[id={getID()}, name={getName()}, salary={getSalary()}]";
+        }
+    }
+}
---
+++ #4 Employee Class/#4 Employee Class/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_Employee_Class
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee(1, "Muhammad", "Ramadian Ramadhan", 3500000);
+
+            Console.WriteLine($"Employee ID             : {employee.getID()}");
+            Console.WriteLine($"Employee First Name     : {employee.getFirstName()}");
+            Console.WriteLine($"Employee Last Name      : {employee.getLastName()}");
+            Console.WriteLine($"Employee Full Name      : {employee.getName()}");
+            Console.WriteLine($"Employee Salary         : {employee.getSalary()}");
+            employee.setSalary(4000000);
+            Console.WriteLine($"Employee Set Salary To  : {employee.getSalary()}");
+            Console.WriteLine($"Employee Annual Salary  : {employee.getAnnualSalary()}");
+            Console.WriteLine($"Employee Raise Salary   : {employee.raiseSalary(3)}\n");
+
+            Console.WriteLine(employee.toString());
+        }
+    }
+}
