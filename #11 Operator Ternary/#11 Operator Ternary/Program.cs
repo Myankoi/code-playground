e6bd1860d4c954e2f6afcc24a6f74ab32729af81
Myan <146714203+Myankoi@users.noreply.github.com>
2024-09-27 14:53:24 +0700

    initialization

---
+++ #11 Operator Ternary/#11 Operator Ternary/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_Operator_Ternary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool boyIsJomblo = true;
+
+            // if else biasa
+            if (boyIsJomblo == true)
+            {
+                Console.WriteLine("Happy");
+            }
+            else
+            {
+                Console.WriteLine("Stress");
+            }
+
+            // if else dengan ternary opetor
+            string output = (boyIsJomblo == true) ? "Happy" : "Stress";
+
+            Console.WriteLine(output);
+        }
+    }
+}
