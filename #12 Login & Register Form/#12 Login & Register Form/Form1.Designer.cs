54e7d3b0d957106514fc8625deac2c43cdf60142
Myan <146714203+Myankoi@users.noreply.github.com>
2024-10-01 15:01:28 +0700

    init

---
+++ #12 Login & Register Form/#12 Login & Register Form/Form1.Designer.cs
@@ -0,0 +1,389 @@
+﻿namespace _12_Login___Register_Form
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gbRegister = new System.Windows.Forms.GroupBox();
+            this.listPosition = new System.Windows.Forms.DomainUpDown();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.label10 = new System.Windows.Forms.Label();
+            this.tbHandphoneNumber = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.tbRegisterVerifyPassword = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.tbEmail = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tbRegisterPassword = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.pbJenisKelamin = new System.Windows.Forms.PictureBox();
+            this.rbFemale = new System.Windows.Forms.RadioButton();
+            this.rbMale = new System.Windows.Forms.RadioButton();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbRegisterUsername = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.gbLogin = new System.Windows.Forms.GroupBox();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.cbShowPassword = new System.Windows.Forms.CheckBox();
+            this.tbLoginUsername = new System.Windows.Forms.TextBox();
+            this.tbLoginPassword = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.gbRegister.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbJenisKelamin)).BeginInit();
+            this.gbLogin.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbRegister
+            // 
+            this.gbRegister.Controls.Add(this.listPosition);
+            this.gbRegister.Controls.Add(this.btnSubmit);
+            this.gbRegister.Controls.Add(this.label10);
+            this.gbRegister.Controls.Add(this.tbHandphoneNumber);
+            this.gbRegister.Controls.Add(this.label9);
+            this.gbRegister.Controls.Add(this.tbRegisterVerifyPassword);
+            this.gbRegister.Controls.Add(this.label8);
+            this.gbRegister.Controls.Add(this.tbEmail);
+            this.gbRegister.Controls.Add(this.label6);
+            this.gbRegister.Controls.Add(this.tbRegisterPassword);
+            this.gbRegister.Controls.Add(this.label7);
+            this.gbRegister.Controls.Add(this.pbJenisKelamin);
+            this.gbRegister.Controls.Add(this.rbFemale);
+            this.gbRegister.Controls.Add(this.rbMale);
+            this.gbRegister.Controls.Add(this.label5);
+            this.gbRegister.Controls.Add(this.tbRegisterUsername);
+            this.gbRegister.Controls.Add(this.label4);
+            this.gbRegister.Controls.Add(this.tbName);
+            this.gbRegister.Controls.Add(this.label3);
+            this.gbRegister.Location = new System.Drawing.Point(372, 40);
+            this.gbRegister.Name = "gbRegister";
+            this.gbRegister.Size = new System.Drawing.Size(418, 371);
+            this.gbRegister.TabIndex = 1;
+            this.gbRegister.TabStop = false;
+            this.gbRegister.Text = "Registrasi User";
+            // 
+            // listPosition
+            // 
+            this.listPosition.Location = new System.Drawing.Point(133, 301);
+            this.listPosition.Name = "listPosition";
+            this.listPosition.Size = new System.Drawing.Size(259, 22);
+            this.listPosition.TabIndex = 31;
+            this.listPosition.Text = "domainUpDown1";
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(299, 332);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(93, 28);
+            this.btnSubmit.TabIndex = 12;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(23, 301);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(55, 16);
+            this.label10.TabIndex = 28;
+            this.label10.Text = "Position";
+            // 
+            // tbHandphoneNumber
+            // 
+            this.tbHandphoneNumber.Location = new System.Drawing.Point(133, 270);
+            this.tbHandphoneNumber.Name = "tbHandphoneNumber";
+            this.tbHandphoneNumber.Size = new System.Drawing.Size(259, 22);
+            this.tbHandphoneNumber.TabIndex = 27;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(23, 273);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(50, 16);
+            this.label9.TabIndex = 26;
+            this.label9.Text = "No. HP";
+            // 
+            // tbRegisterVerifyPassword
+            // 
+            this.tbRegisterVerifyPassword.Location = new System.Drawing.Point(133, 242);
+            this.tbRegisterVerifyPassword.Name = "tbRegisterVerifyPassword";
+            this.tbRegisterVerifyPassword.Size = new System.Drawing.Size(259, 22);
+            this.tbRegisterVerifyPassword.TabIndex = 25;
+            this.tbRegisterVerifyPassword.UseSystemPasswordChar = true;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(23, 245);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(104, 16);
+            this.label8.TabIndex = 24;
+            this.label8.Text = "Verify Password";
+            // 
+            // tbEmail
+            // 
+            this.tbEmail.Location = new System.Drawing.Point(133, 186);
+            this.tbEmail.Name = "tbEmail";
+            this.tbEmail.Size = new System.Drawing.Size(259, 22);
+            this.tbEmail.TabIndex = 23;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(23, 189);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(41, 16);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "Email";
+            // 
+            // tbRegisterPassword
+            // 
+            this.tbRegisterPassword.Location = new System.Drawing.Point(133, 214);
+            this.tbRegisterPassword.Name = "tbRegisterPassword";
+            this.tbRegisterPassword.Size = new System.Drawing.Size(259, 22);
+            this.tbRegisterPassword.TabIndex = 22;
+            this.tbRegisterPassword.UseSystemPasswordChar = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(23, 217);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(67, 16);
+            this.label7.TabIndex = 21;
+            this.label7.Text = "Password";
+            // 
+            // pbJenisKelamin
+            // 
+            this.pbJenisKelamin.Image = global::_12_Login___Register_Form.Properties.Resources.user;
+            this.pbJenisKelamin.Location = new System.Drawing.Point(311, 29);
+            this.pbJenisKelamin.Name = "pbJenisKelamin";
+            this.pbJenisKelamin.Size = new System.Drawing.Size(81, 112);
+            this.pbJenisKelamin.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbJenisKelamin.TabIndex = 19;
+            this.pbJenisKelamin.TabStop = false;
+            // 
+            // rbFemale
+            // 
+            this.rbFemale.AutoSize = true;
+            this.rbFemale.Location = new System.Drawing.Point(133, 121);
+            this.rbFemale.Name = "rbFemale";
+            this.rbFemale.Size = new System.Drawing.Size(98, 20);
+            this.rbFemale.TabIndex = 18;
+            this.rbFemale.TabStop = true;
+            this.rbFemale.Text = "Perempuan";
+            this.rbFemale.UseVisualStyleBackColor = true;
+            this.rbFemale.CheckedChanged += new System.EventHandler(this.rbFemale_CheckedChanged);
+            // 
+            // rbMale
+            // 
+            this.rbMale.AutoSize = true;
+            this.rbMale.Location = new System.Drawing.Point(133, 95);
+            this.rbMale.Name = "rbMale";
+            this.rbMale.Size = new System.Drawing.Size(82, 20);
+            this.rbMale.TabIndex = 17;
+            this.rbMale.TabStop = true;
+            this.rbMale.Text = "Laki-Laki";
+            this.rbMale.UseVisualStyleBackColor = true;
+            this.rbMale.CheckedChanged += new System.EventHandler(this.rbMale_CheckedChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(23, 95);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(90, 16);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Jenis Kelamin";
+            // 
+            // tbRegisterUsername
+            // 
+            this.tbRegisterUsername.Location = new System.Drawing.Point(133, 29);
+            this.tbRegisterUsername.Name = "tbRegisterUsername";
+            this.tbRegisterUsername.Size = new System.Drawing.Size(172, 22);
+            this.tbRegisterUsername.TabIndex = 15;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(23, 32);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 16);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Username";
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(133, 57);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(172, 22);
+            this.tbName.TabIndex = 14;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 16);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Nama";
+            // 
+            // gbLogin
+            // 
+            this.gbLogin.Controls.Add(this.btnRegister);
+            this.gbLogin.Controls.Add(this.btnLogin);
+            this.gbLogin.Controls.Add(this.cbShowPassword);
+            this.gbLogin.Controls.Add(this.tbLoginUsername);
+            this.gbLogin.Controls.Add(this.tbLoginPassword);
+            this.gbLogin.Controls.Add(this.label2);
+            this.gbLogin.Controls.Add(this.label1);
+            this.gbLogin.Location = new System.Drawing.Point(12, 249);
+            this.gbLogin.Name = "gbLogin";
+            this.gbLogin.Size = new System.Drawing.Size(350, 162);
+            this.gbLogin.TabIndex = 2;
+            this.gbLogin.TabStop = false;
+            this.gbLogin.Text = "Login";
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.Location = new System.Drawing.Point(203, 123);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(93, 28);
+            this.btnRegister.TabIndex = 11;
+            this.btnRegister.Text = "Register";
+            this.btnRegister.UseVisualStyleBackColor = true;
+            this.btnRegister.Click += new System.EventHandler(this.btnRegister_Click);
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(99, 123);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(85, 28);
+            this.btnLogin.TabIndex = 10;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // cbShowPassword
+            // 
+            this.cbShowPassword.AutoSize = true;
+            this.cbShowPassword.Location = new System.Drawing.Point(99, 87);
+            this.cbShowPassword.Name = "cbShowPassword";
+            this.cbShowPassword.Size = new System.Drawing.Size(125, 20);
+            this.cbShowPassword.TabIndex = 9;
+            this.cbShowPassword.Text = "Show Password";
+            this.cbShowPassword.UseVisualStyleBackColor = true;
+            this.cbShowPassword.CheckedChanged += new System.EventHandler(this.cbShowPassword_CheckedChanged);
+            // 
+            // tbLoginUsername
+            // 
+            this.tbLoginUsername.Location = new System.Drawing.Point(99, 31);
+            this.tbLoginUsername.Name = "tbLoginUsername";
+            this.tbLoginUsername.Size = new System.Drawing.Size(226, 22);
+            this.tbLoginUsername.TabIndex = 8;
+            // 
+            // tbLoginPassword
+            // 
+            this.tbLoginPassword.Location = new System.Drawing.Point(99, 59);
+            this.tbLoginPassword.Name = "tbLoginPassword";
+            this.tbLoginPassword.Size = new System.Drawing.Size(226, 22);
+            this.tbLoginPassword.TabIndex = 7;
+            this.tbLoginPassword.UseSystemPasswordChar = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Password";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Username";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(802, 423);
+            this.Controls.Add(this.gbLogin);
+            this.Controls.Add(this.gbRegister);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.gbRegister.ResumeLayout(false);
+            this.gbRegister.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbJenisKelamin)).EndInit();
+            this.gbLogin.ResumeLayout(false);
+            this.gbLogin.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbRegister;
+        private System.Windows.Forms.GroupBox gbLogin;
+        private System.Windows.Forms.PictureBox pbJenisKelamin;
+        private System.Windows.Forms.RadioButton rbFemale;
+        private System.Windows.Forms.RadioButton rbMale;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbRegisterUsername;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnRegister;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.CheckBox cbShowPassword;
+        private System.Windows.Forms.TextBox tbLoginUsername;
+        private System.Windows.Forms.TextBox tbLoginPassword;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox tbHandphoneNumber;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox tbRegisterVerifyPassword;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox tbEmail;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tbRegisterPassword;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.DomainUpDown listPosition;
+    }
+}
+
---
+++ #12 Login & Register Form/#12 Login & Register Form/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _12_Login___Register_Form
+{
+    public partial class Form1 : Form
+    {
+        string jenisKelamin;
+        public Form1()
+        {
+            InitializeComponent();
+            this.Width = 300;
+            gbRegister.Visible = false;
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            if (!gbRegister.Visible)
+            {
+                gbRegister.Visible = true;
+                this.Width = 620;
+            } else
+            {
+                gbRegister.Visible = false;
+                this.Width = 300;
+            }
+        }
+
+        private void cbShowPassword_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbShowPassword.Checked)
+            {
+                tbLoginPassword.UseSystemPasswordChar = false;
+            }
+            else 
+            { 
+                tbLoginPassword.UseSystemPasswordChar = true;
+            }
+        }
+
+        private void rbMale_CheckedChanged(object sender, EventArgs e)
+        {
+            pbJenisKelamin.Image = Properties.Resources.Male;
+            jenisKelamin = "Laki-Laki";
+        }
+
+        private void rbFemale_CheckedChanged(object sender, EventArgs e)
+        {
+            pbJenisKelamin.Image = Properties.Resources.Female;
+            jenisKelamin = "Perempuan";
+        }
+    }
+}
