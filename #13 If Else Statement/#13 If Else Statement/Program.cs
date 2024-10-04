262ccf7e17079b007a8d07600ad8fa2d0b6f4152
Myan <146714203+Myankoi@users.noreply.github.com>
2024-10-04 13:42:02 +0700

    init

---
+++ #13 If Else Statement/#13 If Else Statement/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13_If_Else_Statement
+{
+    internal class Program
+    {
+        static int number;
+        static void Main(string[] args)
+        {
+            // Tugas Ramadian X RPL I ❤
+            Console.WriteLine("Program Menentukan Nilai Ganjil/Genap!");
+            number = int.Parse(Console.ReadLine());
+            numberChecker(number);
+
+            Console.ReadLine();
+        }
+
+        static int numberChecker(int number) 
+        {
+            if (number % 2 == 0 || number % 4 == 0 && number % 3 > 0 )
+            {
+                Console.WriteLine($"Angka {number} merupakan angka genap");
+                return number;
+            }  else if (number % 2 > 0 || number % 4 > 0 && number % 3 == 0)
+            {
+                Console.WriteLine($"Angka {number} merupakan angka ganjil");
+                return number;
+            }
+             else
+            {
+                return number;
+            }
+        }
+    }
+}
